200110037f674ae180289f07a98903bb6036c31e
ayan-b <ayanbanerjee7777@gmail.com>
2020-06-23 11:44:55 +0530

    Add telegram comment section

--- src/components/telegram.js
+++ src/components/telegram.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 
 const TelegramComments = () => {
-    const telegramSiteId = useStaticQuery(
+    const telegramSiteIdQuery = useStaticQuery(
         graphql`
           query {
             site {
@@ -14,8 +14,11 @@ const TelegramComments = () => {
           }
         `
     )
+
+    const telegramSiteId = telegramSiteIdQuery.site.siteMetadata.telegramSiteId;
+
     return (
-        <script async src="https://comments.app/js/widget.js?3" data-comments-app-website={{ telegramSiteId }} data-limit="100"></script>
+        <script async src="https://comments.app/js/widget.js?3" data-comments-app-website={ telegramSiteId } data-limit="100"></script>
     )
 }
 
