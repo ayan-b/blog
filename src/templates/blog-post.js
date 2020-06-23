4d3ab606bca69a37714635aa99ec78b708931bc7
ayan-b <ayanbanerjee7777@gmail.com>
2020-06-23 11:35:07 +0530

    Add telegram comments section

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import TelegramComments from "../components/telegram"
 import { rhythm, scale } from "../utils/typography"
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
@@ -74,6 +75,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
           </li>
         </ul>
       </nav>
+      <TelegramComments></TelegramComments>
     </Layout>
   )
 }
